71b2e9807187fa90c7a8ad3ec2d513f8f8944e0a
samsung1999 <larryosaro@gmail.com>
2023-04-20 06:05:23 +0100

    Alpha Demo
    -Play all songs in directory in a queue
    -Current song playing label
    -Persistent Volume Slider

---
+++ Juke/Juke/Juke.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Juke
+{
+    partial class Juke
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.lblCurrentSong = new System.Windows.Forms.Label();
+            this.volumeBar = new System.Windows.Forms.TrackBar();
+            this.lblVolume = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.volumeBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(299, 199);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(136, 40);
+            this.btnPlay.TabIndex = 2;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // lblCurrentSong
+            // 
+            this.lblCurrentSong.AutoSize = true;
+            this.lblCurrentSong.Location = new System.Drawing.Point(170, 99);
+            this.lblCurrentSong.Name = "lblCurrentSong";
+            this.lblCurrentSong.Size = new System.Drawing.Size(28, 13);
+            this.lblCurrentSong.TabIndex = 5;
+            
+            // 
+            // volumeBar
+            // 
+            this.volumeBar.Location = new System.Drawing.Point(627, 33);
+            this.volumeBar.Maximum = 100;
+            this.volumeBar.Name = "volumeBar";
+            this.volumeBar.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.volumeBar.Size = new System.Drawing.Size(45, 163);
+            this.volumeBar.TabIndex = 6;
+            // 
+            // lblVolume
+            // 
+            this.lblVolume.AutoSize = true;
+            this.lblVolume.Location = new System.Drawing.Point(633, 199);
+            this.lblVolume.Name = "lblVolume";
+            this.lblVolume.Size = new System.Drawing.Size(21, 13);
+            this.lblVolume.TabIndex = 7;
+            this.lblVolume.Text = "0%";
+            // 
+            // Juke
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(769, 266);
+            this.Controls.Add(this.lblVolume);
+            this.Controls.Add(this.volumeBar);
+            this.Controls.Add(this.lblCurrentSong);
+            this.Controls.Add(this.btnPlay);
+            this.Name = "Juke";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Juke Player";
+            ((System.ComponentModel.ISupportInitialize)(this.volumeBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Label lblCurrentSong;
+        private System.Windows.Forms.TrackBar volumeBar;
+        private System.Windows.Forms.Label lblVolume;
+    }
+}
+
---
+++ Juke/Juke/Juke.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Media;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Juke
+{
+    public partial class Juke : Form
+    {
+        // Create a new instance of the MediaPlayer class
+        MediaPlayer player = new MediaPlayer();
+        private bool isPlaying = false;
+
+        private static string directoryPath = @"C:\Users\larry\source\repos\Juke\Juke\All"; // Replace with your directory path
+        private string[] audioFiles = Directory.GetFiles(directoryPath, "*.mp3"); // Get all MP3 files in the directory
+
+        int currentAudioIndex = 0; // Keep track of the current audio file being played
+
+
+
+        // Play the current audio file
+        void PlayCurrentAudio()
+        {
+            if (currentAudioIndex < audioFiles.Length)
+            {
+                player.Open(new Uri(audioFiles[currentAudioIndex])); // Open the audio file
+                player.Play(); // Play the audio file
+                               // Subscribe to the MediaEnded event of the MediaPlayer instance to automatically play the next song
+                player.MediaEnded += (sender, e) => PlayNextAudio();
+                string fileName = Path.GetFileName(audioFiles[currentAudioIndex]); // Get the file name without the path
+                lblCurrentSong.Text = "Current Song: " + fileName;
+              
+            }
+        }
+
+        public  void PlayNextAudio()
+        {
+            if (currentAudioIndex < audioFiles.Length - 1) // Make sure we are not at the end of the list
+            {
+                currentAudioIndex++; // Move to the next audio file
+                PlayCurrentAudio(); // Play the new audio file
+            }
+        }
+
+        public Juke()
+        {
+            InitializeComponent();
+
+            // Retrieve the last saved volume level from the application settings
+            int savedVolume = Properties.Settings.Default.VolumeLevel;
+
+            // Set the volume slider's value to the last saved volume level
+            volumeBar.Value = savedVolume;
+
+            lblVolume.Text = $"{savedVolume}%";
+
+            // Set up event handler for the volume slider
+            volumeBar.ValueChanged += (sender, e) => UpdateVolumeLevel(volumeBar.Value);
+
+        }
+
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            if (isPlaying)
+            {
+                player.Pause();
+                //mediaPlayer.Pause();
+                isPlaying = false;
+                btnPlay.Text= "Play";
+                
+            }
+            else
+            {
+                PlayCurrentAudio();
+                //mediaPlayer.Play();
+                isPlaying = true;
+                btnPlay.Text = "Pause";
+
+            }
+        }
+        // Update the volume level of the MediaPlayer instance
+        public void UpdateVolumeLevel(int volume)
+        {
+            player.Volume = volume / 100.0;
+            lblVolume.Text = $"{volume}%";
+
+            // Save the new volume level to the application settings
+            Properties.Settings.Default.VolumeLevel = volume;
+            Properties.Settings.Default.Save();
+        }
+        
+
+       
+    }
+}
